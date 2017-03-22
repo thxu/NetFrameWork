4d11c0228b1dfb3b60692e655e18171416ddd592
thxu <729468926@qq.com>
2017-03-22 23:53:14 +0800

    初始化

---
+++ NetFrameWork/NetFrameWork.Common/Code/Operational.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameWork.Common.Code
+{
+    /// <summary>
+    /// 操作信息
+    /// </summary>
+    public class Operational
+    {
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        public string Operator { get; set; }
+
+        /// <summary>
+        /// 操作内容
+        /// </summary>
+        public string OperationContent { get; set; }
+
+        /// <summary>
+        /// 操作时间
+        /// </summary>
+        public DateTime OperationDateTime { get; set; }
+    }
+}
---
+++ NetFrameWork/NetFrameWork.Common/Code/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NetFrameWork.Common.Code
+{
+    /// <summary>
+    /// 结果
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class Result
+    {
+        /// <summary>
+        /// 成功/失败
+        /// </summary>
+        [DataMember]
+        public bool IsSucceed { get; set; }
+
+        /// <summary>
+        /// 消息
+        /// </summary>
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
