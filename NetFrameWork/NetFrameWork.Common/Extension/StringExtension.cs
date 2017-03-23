2d0d76b571d25a67d39464efad3c3e26b15445d5
thxu <729468926@qq.com>
2017-03-23 21:52:33 +0800

    添加字符串公共类

---
+++ NetFrameWork/NetFrameWork.Common/Extension/StringExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace NetFrameWork.Common.Extension
+{
+    /// <summary>
+    /// 字符串相关扩展方法
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 字符串是否为空
+        /// </summary>
+        /// <param name="val">要判断的字符串</param>
+        /// <returns>判断结果</returns>
+        public static bool IsNullOrEmpty(this string val)
+        {
+            return string.IsNullOrEmpty(val);
+        }
+
+        /// <summary>
+        /// 字符串是否为空或者空白字符串
+        /// </summary>
+        /// <param name="val">要判断的字符串</param>
+        /// <returns>判断结果</returns>
+        public static bool IsNullOrWhiteSpace(this string val)
+        {
+            return string.IsNullOrWhiteSpace(val);
+        }
+    }
+}
