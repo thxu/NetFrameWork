1424407c362a0af197bb8464a949e9b663a58a3f
thxu <729468926@qq.com>
2017-04-27 23:34:45 +0800

    优化了一下

--- NetFrameWork/NetFrameWork.Common/Code/KeyIdFactory.cs
+++ NetFrameWork/NetFrameWork.Common/Code/KeyIdFactory.cs
@@ -11,6 +11,8 @@ namespace NetFrameWork.Common.Code
     /// </summary>
     public class KeyIdFactory
     {
+        private static int _maxLen = 16;
+
         /// <summary>
         /// Fields
         /// </summary>
@@ -27,15 +29,71 @@ namespace NetFrameWork.Common.Code
         /// <returns>主键ID</returns>
         public static string NewKeyId()
         {
-            return NewKeyId(32);
+            return NewKeyId(_maxLen, null);
+        }
+
+        /// <summary>
+        /// 新的主键ID
+        /// </summary>
+        /// <returns>主键ID</returns>
+        public static string NewKeyId(DateTime time)
+        {
+            return NewKeyId(_maxLen, time);
+        }
+
+        /// <summary>
+        /// 新的主键ID
+        /// </summary>
+        /// <param name="code">业务码</param>
+        /// <returns>主键ID</returns>
+        public static string NewKeyId(string code)
+        {
+            return NewKeyId(_maxLen, null) + code;
         }
 
         /// <summary>
         /// 新的主键ID
         /// </summary>
         /// <param name="length">ID长度(不能小于24)</param>
+        /// <param name="time">创建时间</param>
         /// <returns>主键ID</returns>
-        public static string NewKeyId(int length)
+        public static string NewKeyId(int length, DateTime? time)
+        {
+            if (length <= _maxLen)
+            {
+                length = _maxLen;
+            }
+
+            string strNum = string.Empty;
+            for (int i = _maxLen; i <= length; i++)
+            {
+                strNum += "9";
+            }
+
+            lock (locker)
+            {
+                string str = lastIdentity.ToString().PadLeft(strNum.Length, '0');
+                string str2 = time?.ToString("yyMMddHHmmssfff") ?? DateTime.Now.ToString("yyMMddHHmmssfff");
+
+                lastIdentity++;
+
+                if (lastIdentity > long.Parse(strNum))
+                {
+                    lastIdentity = 0;
+                }
+                Random random = new Random(Common.CreateRandomSeed());
+                var r = random.Next(1000, 9999);
+                return $"{str2}{r}{str}";
+            }
+        }
+
+        /// <summary>
+        /// 新的主键ID
+        /// </summary>
+        /// <param name="code">业务码</param>
+        /// <param name="length">ID长度(不能小于24)</param>
+        /// <returns>主键ID</returns>
+        public static string NewKeyId(string code, int length)
         {
             if (length <= 24)
             {
@@ -61,7 +119,7 @@ namespace NetFrameWork.Common.Code
                 }
                 Random random = new Random(Common.CreateRandomSeed());
                 var r = random.Next(1000000, 9999999);
-                return string.Format("{0}{1}{2}", str2, r, str);
+                return $"{str2}{r}{str}{code}";
             }
         }
     }
--- NetFrameWork/NetFrameWork.Test.Exe/Program.cs
+++ NetFrameWork/NetFrameWork.Test.Exe/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetFrameWork.Common.Code;
+using NetFrameWork.Common.Extension;
+using NetFrameWork.Common.Utility;
 
 namespace NetFrameWork.Test.Exe
 {
@@ -10,6 +13,12 @@ namespace NetFrameWork.Test.Exe
     {
         static void Main(string[] args)
         {
+//            var url = "http://localhost:58112/api/DeviceApi/QueryRelayStatusView";
+//            string deviceId = "123123";
+//            var res = new HttpHelper().HttpPost(url, deviceId,"",Encoding.UTF8, false,false,1000);
+
+            var id = KeyIdFactory.NewKeyId();
+            int rt = 0;
         }
     }
 }
